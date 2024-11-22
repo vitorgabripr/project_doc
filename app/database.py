be4dda972efc96c784afbde80aa8e319ff548d04
Vitor <vitor.gabrielll0245@gmail.com>
2024-11-22 11:41:22 -0300

    melhorando-menu

--- app/database.py
+++ app/database.py
@@ -1,2 +1,1 @@
 # manipulação do arquivo JSON
-
--- app/main.py
+++ app/main.py
@@ -13,11 +13,11 @@ def menu():
         "4": lambda: exit("Saindo do programa...")
     }
     while True:
-        print("\nMenu Principal\n")
+        print("\n=== Menu Principal ===\n")
         print("1. Buscar documentação\n")
         print("2. Adicionar documentação\n")
         print("3. Ver todas documentações\n")
-        print("4. Sair\n")
+        print("4. Sair\n------------\n")
 
 
         escolha = input()
